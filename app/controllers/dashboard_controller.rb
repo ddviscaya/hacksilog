786b69ecd50407899459d431bc8723ed48d10749
Jaye Hernandez <jaye@dealgrocer.com>
2019-07-07 00:05:58 +0800

    Add bars and + button

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,5 +7,11 @@ class DashboardController < ApplicationController
     @prev_day = Date::DAYNAMES[@prev_date.wday]
     @current_day = Date::DAYNAMES[@current_date.wday]
     @next_day = Date::DAYNAMES[@next_date.wday]
+
+    @pet_name = "Corgi Butt"
+
+    @heart_progress = 80
+    @happiness_progress = 65
+    @star_progress = 87
   end
 end
