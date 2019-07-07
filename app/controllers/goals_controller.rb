5e7e327b47fba6f4d5304f0100819b26f60e34e4
ddviscaya <daisyreeviscaya@gmail.com>
2019-07-07 09:12:30 +0800

    Fix

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,6 @@
 class GoalsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
+  # skip_before_action :verify_authenticity_token
   def index
     @prev_date = Time.zone.now - 1.day
     @current_date = Time.zone.now
